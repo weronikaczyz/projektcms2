dc9a0880928fca7bee0f729b39f0f5d6a8ea9e00
Weronika Czyz <viera.czyz@gmail.com>
2018-09-03 11:17:08 +0200

    pages views works

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -69,7 +69,7 @@ class PagesController implements ControllerProviderInterface
      */
     public function index(Application $app, Request $request)
     {
-        $id = (int) $request->get('id', 1);
+        $id = (int)$request->get('id', 1);
 
         $page = $this->_model->getPage($id);
 
@@ -95,77 +95,76 @@ class PagesController implements ControllerProviderInterface
      */
     public function edit(Application $app, Request $request)
     {
-        $id = (int) $request->get('id', 1);
-
+        $id = (int)$request->get('id', 1);
 
 
         $data = array();
-//        if ($app['security.authorization_checker']->isGranted('ROLE_ADMIN')) {
-            // default values:
-            $entry = $this->_model->getPage($id);
-            $form = $app['form.factory']->createBuilder(FormType::class, $entry)
-                ->add(
-                    'idpages', HiddenType::class, array(
-                        'constraints' => array(
-                            new Assert\NotBlank()
-                        )
+        //if ($app['security.authorization_checker']->isGranted('ROLE_ADMIN')) {
+        // default values:
+        $entry = $this->_model->getPage($id);
+        $form = $app['form.factory']->createBuilder(FormType::class, $entry)
+            ->add(
+                'idpages', HiddenType::class, array(
+                    'constraints' => array(
+                        new Assert\NotBlank()
                     )
                 )
-                ->add(
-                    'title', TextType::class, array(
-                        'constraints' => array(
-                            new Assert\NotBlank(), new Assert\Length(
-                                array(
-                                    'min' => 3
-                                )
+            )
+            ->add(
+                'title', TextType::class, array(
+                    'constraints' => array(
+                        new Assert\NotBlank(), new Assert\Length(
+                            array(
+                                'min' => 3
                             )
                         )
                     )
                 )
-                ->add(
-                    'content', TextType::class, array(
-                        'constraints' => array(
-                            new Assert\NotBlank(), new Assert\Length(
-                                array(
-                                    'min' => 2
-                                )
+            )
+            ->add(
+                'content', TextType::class, array(
+                    'constraints' => array(
+                        new Assert\NotBlank(), new Assert\Length(
+                            array(
+                                'min' => 2
                             )
                         )
                     )
                 )
-                ->add(
-                    'published', ChoiceType::class, array(
-                        'choices' => array(
-                            'YES' => 1, 'NO' => 0
-                        ),
-                        'expanded' => true,
-                    )
+            )
+            ->add(
+                'published', ChoiceType::class, array(
+                    'choices' => array(
+                        'YES' => 1, 'NO' => 0
+                    ),
+                    'expanded' => true,
                 )
-                ->add('Submit', SubmitType::class)
-                //->add('password', 'password', array(
-                //  'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 3)))
-                //))
-                ->getForm();
+            )
+            ->add('Submit', SubmitType::class)
+            //->add('password', 'password', array(
+            //  'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 3)))
+            //))
+            ->getForm();
 
 
-            $form->handleRequest($request);
+        $form->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
-                $data = $form->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
 
-                $model = $this->_model->saveEntry($data);
-                if (!$model) {
-                    $app['session']->getFlashBag()->set('success', 'Information was updated sucessfully');
-                    return $app->redirect($app['url_generator']->generate('/'), 301);
-                } else {
-                    $app['session']->getFlashBag()->set('error', 'Ooops! An error occured!');
-                }
+            $model = $this->_model->saveEntry($data);
+            if (!$model) {
+                $app['session']->getFlashBag()->set('success', 'Information was updated sucessfully');
+                return $app->redirect($app['url_generator']->generate('/'), 301);
+            } else {
+                $app['session']->getFlashBag()->set('error', 'Ooops! An error occured!');
             }
+        }
 
 
-//        } else {
-//            return $app->redirect($app['url_generator']->generate('/auth/login'), 301);
-//        }
+        //} else {
+        //  return $app->redirect($app['url_generator']->generate('/auth/login'), 301);
+        //}
 
         return $app['twig']->render('pages/edit.twig', array('form' => $form->createView(), 'item' => 'page'));
     }
@@ -181,109 +180,107 @@ class PagesController implements ControllerProviderInterface
         $data = array();
 
         //if ($app['security']->isGranted('ROLE_ADMIN')) {
-            $form = $app['form.factory']->createBuilder(FormType::class, $data)
-                ->add(
-                    'title', TextType::class, array(
-                        'constraints' => array(
-                            new Assert\NotBlank(), new Assert\Length(
-                                array(
-                                    'min' => 3
-                                )
+        $form = $app['form.factory']->createBuilder(FormType::class, $data)
+            ->add(
+                'title', TextType::class, array(
+                    'constraints' => array(
+                        new Assert\NotBlank(), new Assert\Length(
+                            array(
+                                'min' => 3
                             )
                         )
                     )
                 )
-                ->add(
-                    'content', TextType::class, array(
-                        'constraints' => array(
-                            new Assert\NotBlank(), new Assert\Length(
-                                array(
-                                    'min' => 2
-                                )
+            )
+            ->add(
+                'content', TextType::class, array(
+                    'constraints' => array(
+                        new Assert\NotBlank(), new Assert\Length(
+                            array(
+                                'min' => 2
                             )
                         )
                     )
                 )
-                ->add(
-                    'published', ChoiceType::class, array(
-                        'choices' => array(
-                            'YES' => '1', 'NO' => '0'
-                        ),
-                        'expanded' => true,
-                    )
+            )
+            ->add(
+                'published', ChoiceType::class, array(
+                    'choices' => array(
+                        'YES' => '1', 'NO' => '0'
+                    ),
+                    'expanded' => true,
                 )
-                ->add(
-                    'link', ChoiceType::class, array(
-                        'choices' => array(
-                            'YES' => '1', 'NO' => '0'
-                        ),
-                        'expanded' => true,
-                    )
+            )
+            ->add(
+                'link', ChoiceType::class, array(
+                    'choices' => array(
+                        'YES' => '1', 'NO' => '0'
+                    ),
+                    'expanded' => true,
                 )
-                ->add('Submit', SubmitType::class)
-                ->getForm();
+            )
+            ->add('Submit', SubmitType::class)
+            ->getForm();
 
-            $form->handleRequest($request);
+        $form->handleRequest($request);
 
-            if ($form->isValid()) {
-                $entry = $this->_model->newPage($form->getData());
-                if ($entry != NULL) {  // entry should contain id of newly created page or NULL
-                    $link = $form['link']->getData(); // converting data from form to string
-                    if (strcmp($link, 'YES') == 0) { // if user wants to create a link automatically, lets do it!
-                        $menu = new MenuModel($app);// creating new instance of menu model
-                        $title = $form['title']->getData(); // converting data from form to strig
-                        $active = $form['published']->getData(); // as above
-                        $data = array( // assigning default values to an array
-                            'url' =>'/pages/display/'.$entry,
-                            'description' => $title,
-                            'position' => 'LEFT',
-                            'active' => $active,
-                            'external' => 'NO'
-                        );
-                        $success = $menu -> newEntry($data); // database insertion
-                        if (!$success) { // if successful
-                            if ($app['setup']==true) {   // setup mode is on
-                                $app['session']->getFlashBag()->set(
-                                    'success', 'Congratulations! Your new page is ready to use!'
-                                );
-
-                                $global = new SettingsController($app);
-                                $stop = $global->stopSetup($app);
-
-                                return $app->redirect($app['url_generator']->generate('/'), 301);
-                            } else { // setup is off
-                                $app['session']->getFlashBag()->set(
-                                    'success', 'New entry was created along with the link!'
-                                );
-                                return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
-                            }
-                        } else { // if not succesfull
-                            $app['session']->getFlashBag()->set('error', 'Ooops! An error occured! Link was not created!');
-                        }
-                    } else { // user does not want a link
-                        if ($app['setup']==true) {   // setup mode is on
-                            $app['session']->getFlashBag()->set(
-                                'success', 'Congratulations! Your new page is ready to use!'
-                            );
-                            return $app->redirect($app['url_generator']->generate('/'), 301);
-                        } else { // setup is off
-                            $app['session']->getFlashBag()->set('success', 'New entry was created!');
-                            return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
-                        }
-                    }
-                } else { // entry is NULL, which means that new page was not created
-                    $app['session']->getFlashBag()->set(
-                        'error', 'Ooops! An error occured! Neither entry or link were created!'
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entry = $this->_model->newPage($form->getData());
+            if ($entry != NULL) {  // entry should contain id of newly created page or NULL
+                $link = $form['link']->getData(); // converting data from form to string
+                if (strcmp($link, 'YES') == 0) { // if user wants to create a link automatically, lets do it!
+                    $menu = new MenuModel($app);// creating new instance of menu model
+                    $title = $form['title']->getData(); // converting data from form to strig
+                    $active = $form['published']->getData(); // as above
+                    $data = array( // assigning default values to an array
+                        'url' => '/pages/display/' . $entry,
+                        'description' => $title,
+                        'position' => 'LEFT',
+                        'active' => $active,
+                        'external' => 'NO'
                     );
+                    $success = $menu->newEntry($data); // database insertion
+                    if (!$success) { // if successful
+                        // if ($app['setup']==true) {   // setup mode is on
+                        $app['session']->getFlashBag()->set(
+                            'success', 'Congratulations! Your new page is ready to use!'
+                        );
+
+                        $global = new SettingsController($app);
+                        $stop = $global->stopSetup($app);
+
+                        return $app->redirect($app['url_generator']->generate('/'), 301);
+                    } else { // setup is off
+                        $app['session']->getFlashBag()->set(
+                            'success', 'New entry was created along with the link!'
+                        );
+                        //return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
+                    }
+                } else { // if not succesfull
+                    $app['session']->getFlashBag()->set('error', 'Ooops! An error occured! Link was not created!');
+                }
+            } else { // user does not want a link
+                if ($app['setup'] == true) {   // setup mode is on
+                    $app['session']->getFlashBag()->set(
+                        'success', 'Congratulations! Your new page is ready to use!'
+                    );
+                    return $app->redirect($app['url_generator']->generate('/'), 301);
+                } else { // setup is off
+                    $app['session']->getFlashBag()->set('success', 'New entry was created!');
+                    // return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
                 }
             }
-
-        if ($app['setup'] == true) {
-            return $app['twig']->render('setup/new.twig', array('form' => $form->createView()));
-        } else {
-            return $app['twig']->render('pages/new.twig', array('form' => $form->createView()));
+        } else { // entry is NULL, which means that new page was not created
+            $app['session']->getFlashBag()->set(
+                'error', 'Ooops! An error occured! Neither entry or link were created!'
+            );
         }
+
+        return $app['twig']->render('pages/new.twig', array('form' => $form->createView()));
     }
+
+
+
 
     /**
      * This method allows administrator to delete page.
@@ -291,47 +288,47 @@ class PagesController implements ControllerProviderInterface
      * @param application
      * @param request
      */
+
     public function delete(Application $app, Request $request)
     {
-        $id = (int) $request->get('id', 0);
+        $id = (int)$request->get('id', 0);
 
         //if ($app['security']->isGranted('ROLE_ADMIN')) {
-            $form = $app['form.factory']->createBuilder(FormType::class)
-                ->add(
-                    'Yes', SubmitType::class
-                )
-                ->add(
-                    'No', SubmitType::class
-                )
-                ->getForm();
+        $form = $app['form.factory']->createBuilder(FormType::class)
+            ->add(
+                'Yes', SubmitType::class
+            )
+            ->add(
+                'No', SubmitType::class
+            )
+            ->getForm();
 
-            $form->handleRequest($request);
+        $form->handleRequest($request);
 
-            if ($form->isValid()) { // if the form is valid, getting its data
-                $data = $form->getData();
-                if ($form->get('Yes')->isClicked()) { // checking which button was clicked
-                    $delete = $this->_model->deletePage($id); // if yes, proceed to deleting entry
-                    if (!$delete) { // if successful
-                        $app['session']->getFlashBag()->set('success', 'Entry was deleted successfully!');
-                        return $app->redirect(
-                            $app['url_generator']->generate(
-                                '/pages/admin'
-                            ),
-                            301
-                        ); // redirecting to main menu admin site
-                    } else { // if not
-                        $app['session']->getFlashBag()->set('error', 'Ooops! An error occured!');
-                    }
-                } else { // this happens when 'No' is clicked
-                    return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
-                } // redirecting to main menu admin site
-            }
-         else { // if user is not admin
-            return $app->redirect($app['url_generator']->generate('/auth/login'), 301);
+        if ($form->isSubmitted() && $form->isValid()) { // if the form is valid, getting its data
+            $data = $form->getData();
+            if ($form->get('Yes')->isClicked()) { // checking which button was clicked
+                $delete = $this->_model->deletePage($id); // if yes, proceed to deleting entry
+                if (!$delete) { // if successful
+                    $app['session']->getFlashBag()->set('success', 'Entry was deleted successfully!');
+                    return $app->redirect(
+                        $app['url_generator']->generate(
+                            '/pages/admin'
+                        ),
+                        301
+                    ); // redirecting to main menu admin site
+                } else { // if not
+                    $app['session']->getFlashBag()->set('error', 'Ooops! An error occured!');
+                }
+            } else { // this happens when 'No' is clicked
+                return $app->redirect($app['url_generator']->generate('/pages/admin'), 301);
+            } // redirecting to main menu admin site
         }
-        return $app['twig']->render('global/delete.twig', array('form' => $form->createView(), 'item' => 'page'));
+        //     else { // if user is not admin
+        //      return $app->redirect($app['url_generator']->generate('/auth/login'), 301);
+        // }
+        return $app['twig']->render('pages/delete.twig', array('form' => $form->createView(), 'item' => 'page'));
     }
 
-
-
 }
+
